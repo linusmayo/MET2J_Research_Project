0392db7204abcabe4013f50028b640cfb86fb10c
matteovando <156784535+matteovando@users.noreply.github.com>
2024-01-22 17:05:59 +0100

    Not occupations but instruments!

--- Research_Project.py
+++ Research_Project.py
@@ -5,15 +5,15 @@ letters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","
 
 relevantKeys = []
 total_dictionary = {}
-occupations = ("Drummer", "Artist", "Musician", "Rapping")
+instruments = ("Drums", "Guitar", "Piano", "Keyboard", "Voice", "Bass","Violin","Cello","Bass guitar")
 
 for letter in letters:
      file = open(f"{letter}_people.json")
      people = json.load(file)
      for person in people:
-         if ("ontology/birthDate" or "ontology/birthYear") and ("artist" or "ontology/associatedMusicalArtist_label" or "ontology/occupation_label" or "ontology/profession_label") and ("ontology/birthPlace_label" or "ontology/nationality_label" or "ontology/stateOfOrigin_label") and ("ontology/instrument_label" or "ontology/occupation_label") in person:
-            for occupation in occupations:
-                if person["ontology/occupation_label"] == occupation:
+         if ("ontology/birthDate" or "ontology/birthYear") and ("ontology/birthPlace_label" or "ontology/nationality_label" or "ontology/stateOfOrigin_label") and "ontology/instrument_label" in person:
+            for instrument in instruments:
+                if person["ontology/instrument_label" ] == instrument:
                         
                         if (person["ontology/birthDate"] >= datetime.date(1/1/1900) and person["ontology/birthDate"] <= datetime.date(31/12/1999)): 
                             total_dictionary["20th Century"] = 
