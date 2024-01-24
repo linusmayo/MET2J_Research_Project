a8b6a75e3c5b8626179e5e5a0350eac638bf3c7c
matteovando <156784535+matteovando@users.noreply.github.com>
2024-01-24 23:12:31 +0100

    Late night commit yall. Keeping on the grind

--- MET2J final project.R
+++ MET2J final project.R
@@ -1,37 +1,52 @@
 library(tidyverse)
 library(tidyr)
-full_data <-read_csv('MET2J_filtered_instruments.csv')
+
+#------------------------------------GENRES-------------------------------------
+
 genre_data <- read_csv("Genre.csv")
 
 genres_filter <- genre_data |>
   pivot_longer(cols=-'Primary Genre', names_to="Year", values_to="Percentage") |>
-  mutate(Year1 = as.numeric(Year)) |>
-  filter(Year1 >= 1960, Year1 <= 2000) |>
-  mutate(Decade1 = round(Year1/5)*5) |>
-  select(-Year, -Year1) |>
-  mutate(Percentage2 = as.numeric(Percentage))
-  group_by("Decade1") |>
-  summarise(sum_perc = sum(Percentage2))
-  
+  mutate(Year = as.numeric(Year)) |>
+  filter(Year >= 1958, Year <= 2002) |>
+  mutate(Quinquiennial = round(Year/5)*5) |>
+  select(-Year) |>
+  mutate(Percentage = as.numeric(str_replace(Percentage, "%", ""))) |>
+  mutate(Percentage = if_else(is.na(Percentage), 0, Percentage)) |>
+  group_by(Quinquiennial, `Primary Genre`) |>
+  summarise(Percentage = mean(Percentage)) |>
+  pivot_wider(names_from="Primary Genre", values_from = "Percentage")
+
+ggplot(data = genres_filter, aes(x = Quinquiennial)) + 
+  geom_line(aes(y= `House/Electronic/Trance`), color = "navyblue") +
+  geom_line(aes(y= Jazz), color = "gold") +
+  geom_line(aes(y= Pop), color = "purple3") +
+  geom_line(aes(y= Rock), color = "forestgreen") 
 
 print(genres_filter)
 
+#----------------------------------INSTRUMENTS----------------------------------
+
+#Reading in the data from the csv with the data we extracted from Wiki source
+full_data <-read_csv('MET2J_filtered_instruments.csv')
+
 filtered_full_data <- full_data |>
   na.omit() |>
-  mutate(Year2 = as.numeric(`Date of Birth`)) |>
-  filter(Year2>= 1960, Year2 <= 2000) |>
-  mutate(Decade2 = round(Year2/5)*5) |>
+  mutate(`Date of Birth` = as.numeric(`Date of Birth`)) |>
+  filter(`Date of Birth` >= 1960, `Date of Birth` <= 2000) |>
+  mutate(Quinquiennial = round(`Date of Birth`/5)*5) |>
+  select(-`Date of Birth`) |>
   mutate(Instrument = recode(Instrument,"Singing"= "Voice", "Microphone" = "Voice", 
-                                                    "Lead vocalist" = "Voice", "Human voice" = "Voice", 
-                                                    "Vocal music" = "Voice", "Soprano" = "Voice", 
-                                                    "Rapping" = "Voice", "Singer-songwriter"="Voice",
-                                                    "Tenor" = "Voice", "Scat singing" = "Voice",
-                                                    "Vocal percussion" = "Voice",  
-                                                    "Vocoder"="Voice", "Beatboxing"="Voice")) |>
+                             "Lead vocalist" = "Voice", "Human voice" = "Voice", 
+                             "Vocal music" = "Voice", "Soprano" = "Voice", 
+                             "Rapping" = "Voice", "Singer-songwriter"="Voice",
+                             "Tenor" = "Voice", "Scat singing" = "Voice",
+                             "Vocal percussion" = "Voice",  
+                             "Vocoder"="Voice", "Beatboxing"="Voice")) |>
   mutate(Instrument = recode(Instrument,"pianist" = "Piano", "Steinway & Sons" = "Piano", 
-                            "Rhodes piano" = "Piano", "Baldwin Piano Company" = "Piano", 
-                            "List of Korg products" = "Piano", "Keyboard instrument" = "Piano", 
-                            "Fairlight CMI" = "Piano")) |>
+                             "Rhodes piano" = "Piano", "Baldwin Piano Company" = "Piano", 
+                             "List of Korg products" = "Piano", "Keyboard instrument" = "Piano", 
+                             "Fairlight CMI" = "Piano")) |>
   mutate(Instrument = recode(Instrument, "Guitarist" = "Guitar", "PRS guitars" = "Guitar", 
                              "Ibanez" = "Guitar", "Acoustic guitar" = "Guitar", 
                              "ESP Guitars" = "Guitar", "Yamaha electric guitar models" = "Guitar", 
@@ -66,42 +81,28 @@ filtered_full_data <- full_data |>
                              "Digital audio workstation" = "Music software", "Programmer" = "Music software", 
                              "Game Boy music" = "Music software")) |>
   mutate(Instrument = recode(Instrument, "Drum" = "Drums" , "Bass drum" = "Drums", 
-                               "Drum machine" = "Drums", "Sabian" = "Drums", 
-                               "Drum Workshop" = "Drums", "Drum" = "Drums", 
-                               "Gretsch Drums" = "Drums", "Percussion instrument" = "Drums", 
-                               "Noble & Cooley" = "Drums", "Tom-tom drum" = "Drums", 
-                               "Mapex Drums" = "Drums", "Drum kit" = "Drums")) |>
+                             "Drum machine" = "Drums", "Sabian" = "Drums", 
+                             "Drum Workshop" = "Drums", "Drum" = "Drums", 
+                             "Gretsch Drums" = "Drums", "Percussion instrument" = "Drums", 
+                             "Noble & Cooley" = "Drums", "Tom-tom drum" = "Drums", 
+                             "Mapex Drums" = "Drums", "Drum kit" = "Drums")) |>
   mutate(Instrument = recode(Instrument, "Types of trombone" = "Trombone", "C.G. Conn" = "Trombone")) |>
-  write_csv("Cleaned_Instruments.csv")
-
-Filtered_instruments <- filtered_full_data |>
+  #Choosing the relevant instruments 
   filter(Instrument == "Piano" | Instrument == "Guitar" | Instrument =="Music software" | 
-           Instrument == "Voice" | Instrument == "Drums")
-
-
-plot_instruments <- Filtered_instruments |>
-  group_by(Decade2, Instrument) |>
+           Instrument == "Voice") |>
+  #Counting instruments per every five years to plot e.g. 'total guitars in 1965' 
+  group_by(Quinquiennial, Instrument) |>
   summarize(count1 = n()) |>
   ungroup()
 
-print(genres_filter)
-
-
-
-ggplot(data = plot_instruments) + 
-  aes(x = Decade2, y = count1, color = Instrument, group = Instrument) +
+#Each instrument is a line, x-axis is years (every 5), y-axis is number of instruments
+ggplot(data = filtered_full_data) + 
+  aes(x = Quinquiennial, y = count1, color = Instrument, group = Instrument) +
   geom_line() +
   labs(title = "Instrument Counts in the 20th Century",
-       x = "Decade",
+       x = "Quinquiennial",
        y = "Instrument Counts") +
   theme(plot.title = element_text(face = "bold", hjust = 0.5))
-ggsave("Instrument count per decade.pdf")
-
-
-ggplot(data = pivoted_genre_dataset) + 
-  aes(x = Decade1, y = Percentage, color = Genre) +
-  geom_line() 
-
 
 
 
